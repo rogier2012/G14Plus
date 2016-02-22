f0bba123d9cf391f4ee60892142956ac5d464661
Rogier Monshouwer <r.monshouwer@student.utwente.nl>
2016-02-22 13:07:02 +0100

    stub implementation for color refinement

--- color_refinement/color_refinement_algorithm.py
+++ color_refinement/color_refinement_algorithm.py
@@ -4,7 +4,28 @@ from assets.fastgraphs import graph
 def refine(g):
     V = g.V()
 
+    alphalist = []
+    initiallist = []
+    resultlist = []
     for i in range(len(V)):
-        g[i].colornum = 1
+        initiallist.append(i)
+        V[i].colornum = 0
+
+    alphalist.append(initiallist)
+
+    while alphalist != resultlist:
+        resultlist = []
+        for colorlist in range(len(alphalist)):
+            for k in range(len(colorlist)):
+                if k+1 <= len(colorlist):
+                    if len(colorlist[k].nbs()) != len(colorlist[k+1]):
+#                     put k in new color in result list with same properties or create a new list inside result list
+                        pass
+                    elif colorlist[k].nbs().colornum !=  colorlist[k+1].nbs().colornum:
+#                     put k in new color in result list with same properties or create a new list inside result list
+                        pass
+
+
+
 
 
