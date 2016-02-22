b658622bb6904600a3bfd732380308b719aaab0b
Rogier Monshouwer <r.monshouwer@student.utwente.nl>
2016-02-22 13:29:46 +0100

    possible succes

---
+++ assets/testfile.py
@@ -0,0 +1,6 @@
+
+
+
+list1 = [1,2,3]
+
+for i in range(len(list1)) and i is not 2:
--- color_refinement/color_refinement_algorithm.py
+++ color_refinement/color_refinement_algorithm.py
@@ -8,24 +8,31 @@ def refine(g):
     initiallist = []
     resultlist = []
     for i in range(len(V)):
-        initiallist.append(i)
+        resultlist.append(i)
         V[i].colornum = 0
 
-    alphalist.append(initiallist)
+    resultlist.append(initiallist)
 
     while alphalist != resultlist:
+        alphalist = resultlist
         resultlist = []
         for colorlist in range(len(alphalist)):
-            for k in range(len(colorlist)):
-                if k+1 <= len(colorlist):
-                    if len(colorlist[k].nbs()) != len(colorlist[k+1]):
-#                     put k in new color in result list with same properties or create a new list inside result list
-                        pass
-                    elif colorlist[k].nbs().colornum !=  colorlist[k+1].nbs().colornum:
-#                     put k in new color in result list with same properties or create a new list inside result list
-                        pass
+            for k in range(len(colorlist) -1 ):
+                if not same_color(colorlist[k], colorlist[k+1]):
+                    nolistfound = True;
+                    v = colorlist[k+1]
+                    for i in resultlist:
+                        if i != colorlist:
+                            if same_color(i[0],v):
+                                i.append(v)
+                                nolistfound = False;
+                    if nolistfound:
+                        newcolor = [v]
+                        resultlist.append(newcolor)
+
+    return alphalist
 
 
-
-
-
+def same_color(u,v):
+    return len(u.nbs()) == len(v.nbs()) and u.nbs().colornum == v.nbs().colornum
+#put k in new color in result list with same properties or create a new list inside result list
