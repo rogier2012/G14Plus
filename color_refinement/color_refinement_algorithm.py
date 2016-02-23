f483b92caae0192f0046d886f70001598ce58547
Rogier Monshouwer <r.monshouwer@student.utwente.nl>
2016-02-23 21:53:07 +0100

    possible succes #2

--- color_refinement/color_refinement_algorithm.py
+++ color_refinement/color_refinement_algorithm.py
@@ -1,4 +1,5 @@
 from assets.fastgraphs import graph
+from assets.graphIO import loadgraph, writeDOT
 
 
 def refine(g):
@@ -7,32 +8,48 @@ def refine(g):
     alphalist = []
     initiallist = []
     resultlist = []
-    for i in range(len(V)):
-        resultlist.append(i)
-        V[i].colornum = 0
+    for i in V:
+        initiallist.append(i)
+        i.colornum = 0
 
     resultlist.append(initiallist)
-
     while alphalist != resultlist:
         alphalist = resultlist
         resultlist = []
-        for colorlist in range(len(alphalist)):
-            for k in range(len(colorlist) -1 ):
+        for colorlist in alphalist:
+            for k in range(len(colorlist) - 1):
                 if not same_color(colorlist[k], colorlist[k+1]):
-                    nolistfound = True;
-                    v = colorlist[k+1]
+                    nolistfound = True
+                    v = colorlist[k]
                     for i in resultlist:
                         if i != colorlist:
                             if same_color(i[0],v):
                                 i.append(v)
-                                nolistfound = False;
+                                v.colornum = resultlist.index(i)
+                                nolistfound = False
                     if nolistfound:
                         newcolor = [v]
                         resultlist.append(newcolor)
+                        v.colornum = resultlist.index(newcolor)
 
-    return alphalist
+    return G
 
 
 def same_color(u,v):
-    return len(u.nbs()) == len(v.nbs()) and u.nbs().colornum == v.nbs().colornum
+    return len(u.nbs()) == len(v.nbs()) and same_color_neighbour(u,v)
 #put k in new color in result list with same properties or create a new list inside result list
+
+
+def same_color_neighbour(u,v):
+    S = set()
+    T = set()
+    for vertex in u.nbs():
+        S.add(vertex.colornum)
+    for vertex in v.nbs():
+        T.add(vertex.colornum)
+    return S == T
+
+L = loadgraph("colorref_smallexample_4_7.grl",graphclass=graph,readlist=True)
+G = L[0][0]
+G = refine(G)
+writeDOT(G, "examplegraph.dot")
