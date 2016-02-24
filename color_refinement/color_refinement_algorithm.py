d7bda54a76abb15c9f3fd9adb83ba1c74ec09122
Rogier Monshouwer <r.monshouwer@student.utwente.nl>
2016-02-24 14:57:11 +0100

    begin individual refinment

--- color_refinement/color_refinement_algorithm.py
+++ color_refinement/color_refinement_algorithm.py
@@ -1,5 +1,8 @@
+import time
+
 from assets.fastgraphs import graph
 from assets.graphIO import loadgraph, writeDOT
+from assets.graphfunctions import disjointunion
 
 
 def refine(G):
@@ -21,7 +24,6 @@ def refine(G):
         for color_list in alpha_list:
             initial_list = [color_list[0]]
             result_list.append(initial_list)
-
             for k in range(len(color_list) - 1):
                 if not same_color(color_list[k], color_list[k + 1]):
                     no_list_found = True
@@ -41,7 +43,7 @@ def refine(G):
                     v = color_list[k + 1]
                     for i in result_list:
                         if same_color(i[0], v):
-                                i.append(v)
+                            i.append(v)
 
         for color_list in result_list:
             for vertex in color_list:
@@ -49,8 +51,15 @@ def refine(G):
 
     return alpha_list
 
-#put k in new color in result list with same properties or create a new list inside result list
 
+def individual_refinement(G, D, I):
+    pass
+
+
+def countIsomorphism(D, I):
+    alpha1 = individual_refinement(G, D, I)
+
+    pass
 
 def same_color(u, v):
     S = []
@@ -64,15 +73,33 @@ def same_color(u, v):
     # print("Two sets with vertices: " + str(u) +" and " + str(v))
     # print(str(u) + " with set: " + str(S))
     # print(str(v) + " with set: " + str(T))
-
     return S == T
 
 
-L = loadgraph("../graphs/colorref_smallexample_4_7.grl", graphclass=graph, readlist=True)
-G = L[0][2]
-colorschemes = []
-# for graph in L[0]:
-#     colorschemes.append(refine(graph))
+def balanced(alpha):
+    even = True
+    for colorlist in alpha:
+        if len(colorlist) % 2 == 1:
+            even = False
 
-refine(G)
-writeDOT(G, "examplegraph.dot")
+    return even
+
+
+def bijection(alpha):
+    more = True
+    for color_list in alpha:
+        if len(color_list) >= 4:
+            more = False
+    return more
+
+L = loadgraph("../graphs/colorref_smallexample_4_7.grl", graphclass=graph, readlist=True)
+G = L[0][0]
+H = L[0][2]
+GH = disjointunion(G, H)
+
+t1 = int(round(time.time() * 1000))
+alpha1 = refine(GH)
+print("Time runned: " + str(int(round(time.time() * 1000)) - t1) + "ms")
+print("Graph is balanced: " + str(balanced(alpha1)))
+print("Graph is bijection: " + str(bijection(alpha1)))
+writeDOT(GH, "examplegraph.dot")
